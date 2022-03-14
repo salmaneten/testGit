aa1bc7ac16ecacc9aeef705cd1f52cb31f062a74
Salmane Zerradi <szerradi@SMB.SQLI.COM>
2022-03-14 15:41:20 +0100

    added newCat

--- Animal.java
+++ Animal.java
@@ -11,6 +11,9 @@ public class Animal {
     public static String getDogGreeting(){
         return"howhow";
     }
+    public static String getNewCatGreeting(){
+        return"MeowMeow";
+    }
     public static void main(String[] args) {
         String firstArgument = args[0];
         String output = "";
@@ -22,6 +25,12 @@ public class Animal {
         }
         else if(firstArgument.equalsIgnoreCase("dog")){
             output = getDogGreeting();
+        }
+        else if(firstArgument.equalsIgnoreCase("newCat")){
+            output = getNewCatGreeting();
+        }
+        else{
+            output = getDafultGreeting();
         }
         System.out.println(output);
     }
